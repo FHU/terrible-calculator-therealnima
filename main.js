2c14cd5c3c708dba4b59b0a901912ca2652be7a0
therealnima <117394462+therealnima@users.noreply.github.com>
2025-08-27 22:51:26 -0500

    I did it

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-// Step 1 - Find all the elements we need to interact with
-const num1Input = document.getElementById('number1')
-const num2Input = document.getElementById('number2')
-const operatorSelect = document.getElementById('operator')
-const calculateButton = document.getElementById('calculateButton')
-const resultSpan = document.getElementById('result')
 
-// Step 2 - Add an event listener to the button to call a function when clicked
+// step 1 - find all the elements to interact with
+const num1input= document.getElementById('number1')
+const num2input = document.getElementById('number2')
+const calculateButton = document.getElementById('calculateButton')
+const operatorselect = document.getElementById('operator')
+const resultSpan= document.getElementById('result')
+
 calculateButton.addEventListener('click', calculate)
 
 function calculate() {
-    const num1 = parseFloat(num1Input.value)
-    const num2 = parseFloat(num2Input.value)
-    const operator = operatorSelect.value;
+    const num1 = parseFloat(num1input.value )
+    const num2 = parseFloat(num2input.value )
+    const operator=operatorselect.value
 
-    let result
-    if( operator === "+") {
-        result = num1 + num2
+    if (isNaN(num1)|| isNaN(num2)){
+        resultSpan.innerText='one or both numbers are missing'
+        return
     }
-    
-    // Step 3 - update the result span with the result of the calculation
-    resultSpan.innerText = result;
-}
+    let result;
+
+        if(operator==='+'){
+            result = num1+num2
+        }
+        if (operator==='-'){
+            result =num1-num2
+        }
+
+        if(operator==="*"){
+            result = num1*num2
+        }
+
+        if(operator==="/"){
+            if (num2===0){
+                resultSpan.innerText='YO! you can not do that'
+                return
+            }
+        result = num1 / num2
+        }
+    resultSpan.innerText=result
+    }
